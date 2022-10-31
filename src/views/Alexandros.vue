a3cb0a8b05432133b6606cbe02eff4fb78434782
joerivrij <joerivrijaldenhoven@gmail.com>
2022-10-31 14:46:04 +0100

    feat: adds project

---
+++ src/views/Alexandros.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <dictionary-area></dictionary-area>
+  </div>
+</template>
+
+<script>
+import DictionaryArea from "@/components/DictionaryArea";
+
+export default {
+  name: 'Alexandros',
+  data () {
+    return {
+    }
+  },
+  components: {
+    DictionaryArea,
+    'dictionary-area': DictionaryArea
+  },
+  methods: {},
+  computed: {
+  },
+}
+</script>
